463975a1c1dc184fd3be9f538a1bc3b8afe05724
Tyler Billingsley <trbillin@gmail.com>
2026-05-22 10:07:17 -0400

    Hints for function world levels and evaluate tactic

--- Game/Levels/FunctionWorld/L03_Functions.lean
+++ Game/Levels/FunctionWorld/L03_Functions.lean
@@ -8,22 +8,20 @@ Title "function"
 
 Introduction "Let's prove that the compositum of two functions is a function."
 
-
+/-- If `hf : isFunction f`, the `evaluate` tactic lets us "plug in" `a` to `f`. For instance, `evaluate hf at a with b hb` will make the output `b` with `hb` being the information required for `f(a) = b`. -/
+TacticDoc evaluate
 
 Statement {u v w: Type*} (f: Rel u v) (g: Rel v w) (hf: isFunction f) (hg: isFunction g) : isFunction (g ∘ f):= by
   Hint "Start with `intro a` to grab our arbitrary input."
   intro a
   Hint "So, what does `a` map to? Of course, it's g(f(a)), but we have to carefully grab it using our assumptions. In particular, if you try something like `g (f a)`, Lean just gets confused.
 
-  Let's start by grabbing f(a). Intuitively, this is the unique `b` such that `f a b` is true. So we start by plugging `a` into `hf` to say we want the output corresponding to `a`: `obtain hfa := hf a`."
+  To get `f(a)`, we'll use a new tactic: `evaluate`. If `hf` is the hypothesis guaranteeing that `f` is a function (as it is here), we can write `evaluate hf at a with b hb`. This grabs `b`, which will play the role of `f(a)`, and sets `hb` to be the properties required for `b` to be `f(a)`. Try it!"
   evaluate hf at a with b hb
-  obtain hfa := hf a
-  Hint "`b` is still locked away in the unique existence statement, so we'll unpack that: `obtain ⟨b, hb⟩ := hfa; dsimp at hb`. (Without the dsimp, Lean will display some weird trivial function evaluations. Don't worry about it too much.)"
-  obtain ⟨b, hb⟩ := hfa; dsimp at hb
   Hint "Now we have `b`, and `hb.left` is the fact that `f(a) = b` (written as `f a b`, of course)!
 
-  Similarly, obtain `c` such that `g(b) = c`. We can do it in one line: `obtain ⟨c, hc⟩ := hg b; dsimp at hc`"
-  obtain ⟨c, hc⟩ := hg b; dsimp at hc
+  Similarly, obtain `c` such that `g(b) = c`."
+  evaluate hg at b with c hc
   Hint "So, intuitively, `g(f(a)) = c`. Let's prove it: `use! c`. Take it from here!"
   use! c
   constructor
