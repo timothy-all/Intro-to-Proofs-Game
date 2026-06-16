be033f3a187356906358a91f9bf506ed18ed87ef
Tyler Billingsley <trbillin@gmail.com>
2026-06-16 15:09:03 -0400

    function_example6 update

--- Game/Levels/FunctionWorld/FunctionWorldExamples/function_example6.lean
+++ Game/Levels/FunctionWorld/FunctionWorldExamples/function_example6.lean
@@ -1,23 +1,22 @@
-import Game.Levels.FunctionWorld.FunctionWorldExamples.function_example2
+import Game.Levels.FunctionWorld.FunctionWorldExamples.function_example5
 
 
 World "FunctionWorldExamples"
-Level 3
+Level 6
 
 Title "f"
 
-Introduction "A function `f` with a right inverse is onto."
+Introduction "A function `f` with a left inverse is one-to-one."
 
 
-Statement {u v: Type*} (f: Rel u v) (hf: isFunction f) (g: Rel v u) (hg: isFunction g) (hfg: (f ∘ g) = Rel_id v) : isSurjective f := by
-  intro b
-  evaluate hg at b with a hag hau
-  use a
-  evaluate hf at a with c hcg hcu
-  obtain fgbc : (f ∘ g) b c := by use a
-  rw [hfg] at fgbc
-  rw [←fgbc] at hcg
-  exact hcg
+Statement {u v: Type*} (f: Rel u v) (hf: isFunction f) (g: Rel v u) (hg: isFunction g) (hgf: (g ∘ f) = Rel_id u) : isInjective f := by
+  intro a b c fac fbc
+  evaluate hg at c with d hdg hdu
+  obtain gfad : (g ∘ f) a d := by use c
+  obtain gfbd : (g ∘ f) b d := by use c
+  rw [hgf] at gfad
+  rw [hgf, ← gfad] at gfbd
+  exact gfbd.symm
 
 
 
